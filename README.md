6a1da01293bf069012df5a21161def89f68102cc
markus <datamastery87@gmail.com>
2021-03-12 20:11:35 +0100

    chatbot final

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Voice2VoiceChatbot
+
+### Installation
+
+1. `conda create -n test python=3.7`
+2. `conda activate test`
+3. `pip install -r requirements.txt`
+4. `python script.py`
+
+Viel Spaß :-)
--- script.py
+++ script.py
@@ -1,4 +1,6 @@
 # import library
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 import pyttsx3
 import speech_recognition as sr
 
@@ -6,25 +8,42 @@ engineio = pyttsx3.init()
 voices = engineio.getProperty("voices")
 engineio.setProperty("rate", 130)  # Aquí puedes seleccionar la velocidad de la voz
 engineio.setProperty("voice", voices[0].id)
-
-
-# Initialize recognizer class (for recognizing the speech)
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
 
 r = sr.Recognizer()
 
-# Reading Microphone as source
-# listening the speech and store in audio_text variable
-
 with sr.Microphone() as source:
-    print("Talk")
-    audio_text = r.listen(source, timeout=3)
-    print("Time over, thanks")
-    # recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
+    for step in range(5):
+        print("Sprich...")
+        audio = r.listen(source, timeout=3)
+        print("Danke!")
+        audio_text = r.recognize_google(audio)
 
-    try:
-        # using google speech recognition
-        text = r.recognize_google(audio_text)
-        engineio.say(text)
-        engineio.runAndWait()
-    except:
-        print("Sorry, I did not get that")
+        new_user_input_ids = tokenizer.encode(
+            audio_text + tokenizer.eos_token, return_tensors="pt"
+        )
+        bot_input_ids = (
+            torch.cat([chat_history_ids, new_user_input_ids], dim=-1)
+            if step > 0
+            else new_user_input_ids
+        )
+        chat_history_ids = model.generate(
+            bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id
+        )
+        print(chat_history_ids.shape)
+        print(type(chat_history_ids))
+        new_text = tokenizer.decode(
+            chat_history_ids[:, bot_input_ids.shape[-1] :][0], skip_special_tokens=True
+        )
+
+        print(new_text)
+        # recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
+
+        try:
+            # using google speech recognition
+            engineio.say(new_text)
+            engineio.runAndWait()
+        except:
+            engineio.say("Sorry, did not understand you")
+
